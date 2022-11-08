80e367c9446545215ccdd2771c80ee0131272f17
sl243 <slmiucse@gmail.com>
2022-11-08 20:31:00 +0600

    added user and password mongodb

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// user: photographyUser
-// pass: Password243
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2elctou.mongodb.net/?retryWrites=true&w=majority`;
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.2elctou.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
